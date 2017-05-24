6351b0616f8bdde367dfd46b4879570f053b9aed
zgx <slys12345>
2017-05-24 18:25:12 +0800

    登录界面修改

--- lib/src/components/singlePage/Login.dart
+++ lib/src/components/singlePage/Login.dart
@@ -16,81 +16,109 @@ class UserData {
 }
 
 class _LoginState extends State<Login> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  TextEditingController _phoneController = new TextEditingController();
+
+  UserData user = new UserData();
+
+  void showInSnackBar(String s) {
+    _scaffoldKey.currentState.showSnackBar(
+        new SnackBar(content: new Text(s))
+    );
+  }
+
+  void _handleSubmitted() {
+    final FormState form = _formKey.currentState;
+    if (!form.validate()) {
+      _autovalidate = true;  // Start validating on every change.
+      showInSnackBar('请按照提示修改输入内容.');
+    } else {
+//        form.save();
+      showInSnackBar('${user.phoneno}\'s phone number is ${user.phoneno}');
+    }
+  }
   String userName;
   String password;
 
+  bool _autovalidate = false;
+  bool _formWasEdited = false;
 
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
-    TextEditingController _phoneController = new TextEditingController();
-
-    UserData user = new UserData();
 
     return new Scaffold(
+//      key: _scaffoldKey,
       appBar: new AppBar(
         title: const Text('登录'),
         centerTitle: true,
       ),
 
-    body: new Form(
-      child: new ListView(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        children: [
-          new Container(
-            child: new TextFormField(
-              decoration: const InputDecoration(
-                icon: const Icon(Icons.phone_iphone),
-                hintText: '请输入手机号码',
-                labelText: '手机号码',
+      body: new Form(
+        key: _formKey,
+//        autovalidate: _autovalidate,
+        child: new ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            children: [
+              new Container(
+                child: new TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.phone_iphone),
+                    hintText: '请输入手机号码',
+                    labelText: '手机号码',
+                  ),
+                  keyboardType: TextInputType.number,
+//                  onSaved: (String value) {
+//                    //form.save时才会调用
+//                    user.phoneno = value;
+//                  },
+                  inputFormatters: [
+                    WhitelistingTextInputFormatter.digitsOnly,
+                  ],
+                  validator: _validatePhoneNumber,
+                ),
               ),
-              keyboardType: TextInputType.number,
-              onSaved: (String value) { //form.save时才会调用
-                user.phoneno = value;
-              },
-              inputFormatters: [
-                WhitelistingTextInputFormatter.digitsOnly,
-              ],
-            ),
-//            decoration: new BoxDecoration(
-//              border: new Border.all(width: 1.0,color: Theme.of(context).dividerColor)
-//            ),
-          ),
-
-          new TextFormField(
-            decoration: const InputDecoration(
-              icon: const Icon(Icons.keyboard),
-              hintText: '请输入登录密码',
-              labelText: '密码'
-            ),
-            keyboardType: TextInputType.number,
-            onSaved: (String value) {
-              user.phoneno = value;
-
-            },
-            inputFormatters: [
-              WhitelistingTextInputFormatter.digitsOnly,
-            ],
-          ),
-
-          new Container(
-            padding: const EdgeInsets.all(20.0),
-            child: new RaisedButton(
-              onPressed: (){},
-              child: const Text('登录'),
-            ),
-          ),
-//          new TextField(
-//            decoration: const InputDecoration(
-//              icon: const Icon(Icons.phone_iphone),
-//              hintText: '请输入手机号码'
-//            ),
-//          ),
-
-          new Image.asset('images/ic_center_more_icon.png', width: 150.0, height: 150.0),
-        ]
+              new TextFormField(
+                decoration: const InputDecoration(
+                    icon: const Icon(Icons.keyboard),
+                    hintText: '请输入登录密码',
+                    labelText: '密码'),
+//                onSaved: (String value) {
+//                  user.phoneno = value;
+//                },
+              ),
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  new Container(
+                    child: new InkWell(
+                      onTap: () {},
+                      child: const Text('忘记密码?'),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 16.0,
+                  ),
+                  new Container(
+                    child: new InkWell(
+                      onTap: () {},
+                      child: const Text('注册'),
+                    ),
+                  ),
+                ],
+              ),
+              new Container(
+                padding: const EdgeInsets.all(20.0),
+                child: new RaisedButton(
+//                  onPressed: (){},
+                  onPressed: _handleSubmitted,
+                  child: const Text('登录'),
+                ),
+              ),
+              new Image.asset('images/ic_center_more_icon.png',
+                  width: 150.0, height: 150.0),
+            ]),
       ),
-    ),
 
       //旧版本
 //      body: new Column(
@@ -138,10 +166,16 @@ class _LoginState extends State<Login> {
   }
 
   String _validatePhoneNumber(String value) {
+    _formWasEdited = true;
+    final RegExp phoneExp = new RegExp(r'^\(\d\d\d\) \d\d\d\-\d\d\d\d$');
+    if (!phoneExp.hasMatch(value))
+      return '(###) ###-#### - 请检查格式.';
     return null;
   }
 
   String _validatePassword(String value) {
     return null;
   }
+
+
 }
